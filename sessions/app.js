7a653f2fdee7bfbaf63841205fe7434cfdea7b03
Taha Shakeel <taha.shakeel@venturedive.com>
2020-02-18 00:10:31 +0500

    Created http server and read file and displayed as response

--- sessions/app.js
+++ sessions/app.js
@@ -1,6 +1,19 @@
-const myModule = require('./logger');
-const myArithmaticModule = require('./arithmatic');
+const http = require('http');
+var fs = require('fs');
+// const myModule = require('./logger');
+// const myArithmaticModule = require('./arithmatic');
 
-const logger = myModule.logger;
-console.log('adding two numbers => 1 + 2 =', myArithmaticModule.addFunction(1,2));
+// const logger = myModule.logger;
+// console.log('adding two numbers => 1 + 2 =', myArithmaticModule.addFunction(1,2));
 // console.log(logger);
+
+const requestListener = function (req, res) {
+  const fileContent = fs.readFileSync('./fileToRead.txt', 'utf8');
+  res.writeHead(200);
+  res.end(fileContent);
+}
+
+const server = http.createServer(requestListener);
+server.listen(4000);
+
+console.log('Express server started on port %s', server.address().port);
